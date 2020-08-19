3646e258cc22f5e53f7bcb699f6720e69d94a23b
Gaunish <anigarg1999@gmail.com>
2020-08-19 19:39:30 +0530

    This commit adds the database and does some changes to application.py

--- application.py
+++ application.py
@@ -1,19 +1,67 @@
-from flask import Flask, render_template, redirect
+#importing all dependencies
+import os
+from flask import Flask, render_template, redirect, g, session, request, flash
+from flask_session import Session
+from tempfile import mkdtemp
+import sqlite3
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
-app = Flask(__name__, static_url_path='/groupstudy')
+#configuring app
+app = Flask(__name__)
 
+#Ensure template are auto reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+#configure session to filesystem
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#Ensure responses arent cached
+@app.after_request
+def after_response(response):
+    response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
+    response.headers['Expires'] = 0
+    response.headers['Pragma'] = "no-cache"
+    return response
+
+#connecting database
+DATABASE = '~/GroupStudy/Groupstudy/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+#Redirects to the front page
 @app.route('/')
 def index():
     return render_template("front.html")
 
 @app.route('/login')
 def redirect():
-    return render_template("login.html")
+    #checking if user has entered via webpage
+    if request.method == "POST":
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 @app.route('/register')
 def redirect1():
     return render_template("register.html")
+
 
 @app.route('/create')
 def redirect2():
